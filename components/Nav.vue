6803b5ac4d154bc45f5bdcf3c6ca5fbf9b2acaea
blastnote <nickbrinthaupt@gmail.com>
2021-05-15 02:03:14 -0400

    Home page update 1

---
+++ components/Nav.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <!-- Top bar -->
+        <div class="w-100 bg-primary text-white text-nowrap">
+            <span class="ml-5 pl-5">
+                <a class="text-white" href="https://g.page/crewittscreek?share">2037 Centennial Blvd, Independence, KY 41051</a> - 
+                <a class="text-white" href="tel:+18593595121">(859) 359-5121</a>
+            </span>
+
+            <span class="mr-5 pr-5 float-right">
+                <a class="mx-2" href="https://www.facebook.com/crewittscreek/"><i class="fab fa-facebook-square text-white"></i></a>
+                <a class="mx-2" href="https://www.instagram.com/crewittscreek/"><i class="fab fa-instagram text-white"></i></a>
+            </span>
+        </div>
+        <!-- nav bar -->
+        <div>
+            <b-navbar toggleable="md" :type="mode" variant="transparent">
+                <b-navbar-brand class="ml-4" to="/">
+                    <img src="~/assets/imgs/ShortTransparent.png" alt="Crewitts Creek" />
+                </b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto mr-5">
+                    <b-nav-item class="mx-4" style="font-size: 1.5rem;" to="/">Home</b-nav-item>
+                    <b-nav-item class="mx-4" style="font-size: 1.5rem;" to="/">Menu</b-nav-item>
+                    <b-nav-item class="mx-4" style="font-size: 1.5rem;" to="/">Drinks</b-nav-item>
+                    <b-nav-item class="mx-4" style="font-size: 1.5rem;" to="/">Contact</b-nav-item>
+                </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        mode: {
+            type: String,
+            default: 'light'
+        },
+        page: {
+            type: String
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="hero w-100">
+            <Nav mode="dark" page="home"/>
+            <div>
+                <p class="text-white" style="font-size: 4rem;">Welcome To The Creek</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.hero {
+    height: 48rem;
+    background-image: linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.4) 20%, rgba(0,0,0,0.4) 80%, rgba(0,0,0,0.7) 100%), url('~/assets/imgs/Hero.jpg');
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
