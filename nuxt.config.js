20d5d2be8b8e40d271f3fb4612491121053f5467
blastnote <nickbrinthaupt@gmail.com>
2021-07-23 03:27:37 -0400

    SEO changes to main header

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,28 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Crewitts Creek',
     htmlAttrs: {
       lang: 'en'
     },
+    title: 'Crewitts Creek',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      { name: 'og:site_name', content: 'Crewitts Creek Kitchen & Bar' },
+      { name: 'og:url', content: 'http://www.crewittscreek.com' },
+      { name: 'og:title', content: 'Crewitts Creek' },
+      { name: 'og:image', content: '~/assets/imgs/ShortTransparent.png' },
+      { name: 'description', content: 'Crewitts Creek Kitchen & Bar is an American restaurant located in Independence, Kentucky' },
+      { name: 'og:street-address', content: '2037 Centennial Blvd' },
+      { name: 'og:locality', content: 'Independence' },
+      { name: 'og:region', content: 'KY' },
+      { name: 'og:postal-code', content: '41051' },
+      { name: 'og:country-name', content: 'USA' }
     ],
     link: [
-      { rel: 'icon', type: 'image/png', href: '/icon.png' }
+      { rel: 'icon', type: 'image/png', href: '/icon.png' },
+      { rel: 'menu', href: 'http://www.crewittscreek.com/menu' },
+      { rel: 'drinks', href: 'http://www.crewittscreek.com/drinks' }
     ],
     script: [
       { src: 'https://kit.fontawesome.com/9ebf1385dd.js', crossorigin: 'anonymous' }
